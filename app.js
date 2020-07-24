c5be0e09cac052dc0c70e39df7b6b382c72342f6
Samuel KUETA <samuel.kueta@gmail.com>
2020-07-24 22:41:02 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express'),
+   bodyParser = require('body-parser'),
+          ejs = require('ejs'),
+        Nexmo = require('nexmo'),
+     socketio = require('socket.io');
+
+// Init Nexmo
+const userCredential = require('./userCredentials');
+
+const nexmo = new Nexmo({
+    apiKey: userCredential.apiKey,
+    apiSecret: userCredential.apiSecret
+}, { debug: true });
+
+// Init app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+// Catch form submit
+app.post('/', (req, res) => {
+    // res.send(req.body);
+    // console.log(req.body);
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+        userCredential.myVirtualNumber, number, text, { type: 'unicode' },
+        (err, responseData) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.dir(responseData);
+                // Get data from response
+                const data = {
+                    id: responseData.messages[0]['message-id'],
+                    number: responseData.messages[0]['to']
+                }
+
+                // Emit to the client
+                io.emit('smsStatus', data);
+            }
+        }
+    );
+});
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+    console.log('Connected');
+    io.on('disconnect', () => {
+        console.log('Disconnected');
+    })
+})
